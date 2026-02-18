8062a7d9bdf426077146369bf245ab0a068502f2
jonier6 <jquiroga@sistemasenlinea.com.co>
2026-02-18 18:25:35 -0500

    add comment minmax

--- minmax/main.go
+++ minmax/main.go
@@ -1,3 +1,5 @@
+//project minmax
+
 package main
 
 import (
@@ -8,13 +10,11 @@ import (
 	"strings"
 )
 
-
 func getInput() string {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	return scanner.Text()
 }
-
 
 func minmax(min, max float64, values ...float64) []float64 {
 	var result []float64
@@ -29,21 +29,18 @@ func minmax(min, max float64, values ...float64) []float64 {
 }
 
 func main() {
-	
+
 	fmt.Print("Ingresa el valor mínimo: ")
 	minStr := getInput()
 	min, _ := strconv.ParseFloat(minStr, 64)
 
-	
 	fmt.Print("Ingresa el valor máximo: ")
 	maxStr := getInput()
 	max, _ := strconv.ParseFloat(maxStr, 64)
 
-	
 	fmt.Print("Ingresa los valores separados por espacio: ")
 	listStr := getInput()
-	
-	
+
 	strFields := strings.Fields(listStr)
 	var floatValues []float64
 	for _, s := range strFields {
@@ -51,9 +48,7 @@ func main() {
 		floatValues = append(floatValues, f)
 	}
 
-	
 	filtered := minmax(min, max, floatValues...)
 
-	
 	fmt.Printf("Resultado: %v\n", filtered)
 }
