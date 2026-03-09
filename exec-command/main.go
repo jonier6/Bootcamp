07d83a022caecc3bea800b673c5b1b993ee5274a
jonier6 <jquiroga@sistemasenlinea.com.co>
2026-03-09 14:26:18 -0500

    2.Running a command and showing output

--- exec-command/main.go
+++ exec-command/main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"log"
 	"os/exec"
+	"os"
 )
 
 func main() {
 
 	cmd := exec.Command("tasklist")
 
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	
 	err := cmd.Run()
 
 	if err != nil {
